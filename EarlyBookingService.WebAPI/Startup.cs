809a834f3ad8c667177f57d270c958140a7758ca
TssBrk <tessa.braeke@gmail.com>
2019-01-19 15:06:24 +0100

    add EarlyBookingController

--- EarlyBookingService.WebAPI/Startup.cs
+++ EarlyBookingService.WebAPI/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EarlyBookingService.WebAPI.Models;
+using EarlyBookingService.WebAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,8 +30,8 @@ namespace EarlyBookingService.WebAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<EarlyBookingServiceContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("EarlyBookingService"))
-            );
+                options.UseSqlServer(Configuration.GetConnectionString("EarlyBookingService")));
+            services.AddScoped<EarlyBookingRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
