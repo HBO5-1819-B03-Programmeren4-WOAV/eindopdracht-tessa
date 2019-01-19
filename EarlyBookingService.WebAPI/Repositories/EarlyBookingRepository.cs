f8b1337d1279cdf3fab7d165f914520922234d21
TssBrk <tessa.braeke@gmail.com>
2019-01-19 14:56:59 +0100

    add earlybookingrepository + registration

---
+++ EarlyBookingService.WebAPI/Repositories/EarlyBookingRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EarlyBookingService.WebAPI.Models;
+
+namespace EarlyBookingService.WebAPI.Repositories
+{
+    public class EarlyBookingRepository
+    {
+        private EarlyBookingServiceContext db;
+
+        public EarlyBookingRepository(EarlyBookingServiceContext context)
+        {
+            db = context;
+        }
+
+        public List<PurchaseCost> ListEBR()
+        {
+            return db.PurchaseCosts.Where(t => t.CostType == CostType.EBR).ToList();
+        }
+    }
+}
