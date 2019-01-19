5391d87260b5c8cee3fbbe16f0d4cb4c103fb24e
TssBrk <tessa.braeke@gmail.com>
2019-01-19 16:08:50 +0100

    update earlybookingrepository

--- EarlyBookingService.WebAPI/Repositories/EarlyBookingRepository.cs
+++ EarlyBookingService.WebAPI/Repositories/EarlyBookingRepository.cs
@@ -7,13 +7,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EarlyBookingService.WebAPI.Repositories
 {
-    public class EarlyBookingRepository
+    public class EarlyBookingRepository: Repository<PurchaseCost>
     {
-        private EarlyBookingServiceContext db;
 
-        public EarlyBookingRepository(EarlyBookingServiceContext context)
+        public EarlyBookingRepository(EarlyBookingServiceContext context) : base(context)
         {
-            db = context;
+
         }
 
         public List<PurchaseCost> ListEBR()
@@ -21,30 +20,5 @@ namespace EarlyBookingService.WebAPI.Repositories
             return db.PurchaseCosts.Where(t => t.CostType == CostType.EBR).ToList();
         }
 
-        public async Task<PurchaseCost> Update(PurchaseCost purchaseCost)
-        {
-            try
-            {
-                db.Entry(purchaseCost).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if(!PurchaseCostExists(purchaseCost.Id))
-                {
-                    return null;
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return purchaseCost;
-        }
-
-        public bool PurchaseCostExists(int id)
-        {
-            return db.PurchaseCosts.Any(e => e.Id == id);
-        }
     }
 }
