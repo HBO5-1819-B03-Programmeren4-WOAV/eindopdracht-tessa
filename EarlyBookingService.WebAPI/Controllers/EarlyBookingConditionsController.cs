35342b773a99298c79b7a2626e776f52b66eb7c7
TssBrk <tessa.braeke@gmail.com>
2019-01-19 15:23:36 +0100

    update earlybooking repository and controller to update purchasecost

---
+++ EarlyBookingService.WebAPI/Controllers/EarlyBookingConditionsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EarlyBookingService.WebAPI.Models;
+using EarlyBookingService.WebAPI.Repositories;
+
+namespace EarlyBookingService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EarlyBookingConditionsController : ControllerBase
+    {
+        EarlyBookingRepository repository;
+
+        public EarlyBookingConditionsController(EarlyBookingRepository ebRepository)
+        {
+            repository = ebRepository;
+        }
+
+        // GET : api/EarlyBookingConditions
+        [HttpGet]
+        public IActionResult GetEBConditions()
+        {
+            return Ok(repository.ListEBR());
+        }
+
+        // PUT : api/EarlyBookingConditions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEarlyBookingCondition([FromRoute] int id, [FromBody] PurchaseCost purchaseCost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != purchaseCost.Id)
+            {
+                return BadRequest();
+            }
+
+            PurchaseCost p = await repository.Update(purchaseCost);
+            if(p == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(p);
+
+        }
+    }
+}
--- EarlyBookingService.WebAPI/Repositories/EarlyBookingRepository.cs
+++ EarlyBookingService.WebAPI/Repositories/EarlyBookingRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EarlyBookingService.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EarlyBookingService.WebAPI.Repositories
 {
@@ -18,6 +19,32 @@ namespace EarlyBookingService.WebAPI.Repositories
         public List<PurchaseCost> ListEBR()
         {
             return db.PurchaseCosts.Where(t => t.CostType == CostType.EBR).ToList();
+        }
+
+        public async Task<PurchaseCost> Update(PurchaseCost purchaseCost)
+        {
+            try
+            {
+                db.Entry(purchaseCost).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!PurchaseCostExists(purchaseCost.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return purchaseCost;
+        }
+
+        public bool PurchaseCostExists(int id)
+        {
+            return db.PurchaseCosts.Any(e => e.Id == id);
         }
     }
 }
