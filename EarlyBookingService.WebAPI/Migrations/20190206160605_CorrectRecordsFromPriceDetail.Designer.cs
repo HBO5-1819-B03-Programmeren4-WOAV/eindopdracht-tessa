3c7326f22181d1bc20a340f5e0b29face3fb4c47
TssBrk <tessa.braeke@gmail.com>
2019-02-06 17:18:18 +0100

    final commit

--- EarlyBookingService.WebAPI/Migrations/20190206160605_CorrectRecordsFromPriceDetail.Designer.cs
+++ EarlyBookingService.WebAPI/Migrations/20190206160605_CorrectRecordsFromPriceDetail.Designer.cs
@@ -4,14 +4,16 @@ using EarlyBookingService.WebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EarlyBookingService.WebAPI.Migrations
 {
     [DbContext(typeof(EarlyBookingServiceContext))]
-    partial class EarlyBookingServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20190206160605_CorrectRecordsFromPriceDetail")]
+    partial class CorrectRecordsFromPriceDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EarlyBookingService.WebAPI/Migrations/20190206160605_CorrectRecordsFromPriceDetail.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EarlyBookingService.WebAPI.Migrations
+{
+    public partial class CorrectRecordsFromPriceDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "PurchaseCostId",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "PurchaseCostId",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "PurchaseCostId",
+                value: 8);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "PurchaseCostId",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "PurchaseCostId",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "PriceDetail",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "PurchaseCostId",
+                value: 7);
+        }
+    }
+}
